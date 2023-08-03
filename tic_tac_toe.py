55f07feb11996d7450bc28b6c5b5abde6d542fca
delisma <david.elisma@tbs-sct.gc.ca>
2023-08-03 09:21:43 -0400

    aider: Added board creation, printing, and win checking functionality to the tic_tac_toe.py script.
    # Aider chat conversation:
    
    USER: Still not running when I type python3 `tic_tac_toe.py` in the console
    
    ASSISTANT: No changes are needed.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,30 @@
+# Create the board
+board = [' ' for _ in range(9)]
+
+# Function to print the board
+def print_board():
+    print('---------')
+    for i in range(3):
+        print('|', board[i*3], '|', board[i*3 + 1], '|', board[i*3 + 2], '|')
+        print('---------')
+
+# Function to check if a player has won
+def check_win(player):
+    # Check rows
+    for i in range(0, 9, 3):
+        if board[i] == board[i+1] == board[i+2] == player:
+            return True
+    # Check columns
+    for i in range(3):
+        if board[i] == board[i+3] == board[i+6] == player:
+            return True
+    # Check diagonals
+    if board[0] == board[4] == board[8] == player:
+        return True
+    if board[2] == board[4] == board[6] == player:
+        return True
+    return False
+
 # Function to play the game
 def play_game():
     current_player = 'X'
@@ -34,3 +61,6 @@ def play_game():
             break
         
         current_player = 'O' if current_player == 'X' else 'X'
+
+# Start the game
+play_game()
